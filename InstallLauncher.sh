57b0581c21c9c7f564480f88b27dc8db0012ddcf
DTnmy <tanmaydutta92@gmail.com>
2021-12-10 21:07:09 +0530

    changed .sh and .desktop

---
+++ InstallLauncher.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+chmod +x InfileGenerator
+cp -r . /opt/InfileGenerator
+cp ./MicroSim.desktop /usr/share/applications/MicroSim.desktop
+echo "Launcher created successfully!"
--- createLauncher.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-sudo chmod +x InfileGenerator
-sudo cp ./MicroSim.desktop /usr/share/applications/MicroSim.desktop
